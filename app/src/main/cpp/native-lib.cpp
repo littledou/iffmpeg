48caa874f16ac137f8c018e9d63fe5da9ed7cc05
dou <app_yoko@163.com>
2019-12-20 16:02:29 +0800

    test link ffmpeg

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,20 @@
+#include <jni.h>
+#include <string>
+
+#ifdef __cplusplus
+extern "C" {
+
+#include <libavcodec/avcodec.h>
+#endif
+
+JNIEXPORT jstring JNICALL Java_cn_readsense_iffmpeg_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = avcodec_configuration();
+    return env->NewStringUTF(hello.c_str());
+}
+
+#ifdef __cplusplus
+};
+
+#endif
